1f2f422a313a57ceef08a3b89a5f4dc38a8c8eb6
Robin Kristiansen <1232179+nibrobb@users.noreply.github.com>
2025-08-04 00:15:26 +0200

    Implement echo.sh as route instead of static file

---
+++ src/routes/echo.sh/+server.ts
@@ -0,0 +1,10 @@
+import type { RequestHandler } from './$types';
+import echo_sh from "$lib/files/echo.sh?raw";
+
+export const GET: RequestHandler = async () => {
+    return new Response(echo_sh, {
+        headers: {
+            "Content-Type": "text/plain; charset=utf-8"
+        }
+    })
+};

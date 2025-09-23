c735b3631c3d45933cfb8d565351f6f8d11b3231
Robin Kristiansen <1232179+nibrobb@users.noreply.github.com>
2025-09-23 22:20:41 +0200

    Change favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon_light from '$lib/assets/favicon_light.png';
+	import favicon_dark from '$lib/assets/favicon_dark.png';
 	import './under-construction.css';
 	import { onMount } from 'svelte';
 
@@ -50,7 +51,9 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href={favicon_light} media="(prefers-color-scheme: dark)"/>
+	<link rel="icon" href={favicon_dark} media="(prefers-color-scheme: light)"/>
+	
 </svelte:head>
 
 {@render children?.()}

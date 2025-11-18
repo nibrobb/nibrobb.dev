9598dfbd241a1b9484d54c9a231112669cdee6f3
Robin Kristiansen <1232179+nibrobb@users.noreply.github.com>
2025-11-18 21:31:50 +0100

    Formatting

--- src/routes/luxafor-ui/+page.svelte
+++ src/routes/luxafor-ui/+page.svelte
@@ -1,45 +1,46 @@
-<script lang="ts">
-    import type { PageProps } from './$types';
+<main>
+	<script lang="ts">
+		import type { PageProps } from './$types';
 
-    let { data }: PageProps = $props();
-</script>
+		let { data }: PageProps = $props();
+	</script>
 
-<style>
-    :root {
-        background-color: #181818;
-    }
-    .heading-text {
-        color: white;
-    }
-    .luxafor-ui-content {
-        display: flex;
-        flex-direction: column;
-        flex-wrap: wrap;
-        align-items: center;
-        position: absolute;
-        left: 50%;
-        top: 50%;
-        -webkit-transform: translate(-50%, -50%);
-        transform: translate(-50%, -50%);
-    }
-</style>
+	<style>
+		:root {
+			background-color: #181818;
+		}
+		.heading-text {
+			color: white;
+		}
+		.luxafor-ui-content {
+			display: flex;
+			flex-direction: column;
+			flex-wrap: wrap;
+			align-items: center;
+			position: absolute;
+			left: 50%;
+			top: 50%;
+			-webkit-transform: translate(-50%, -50%);
+			transform: translate(-50%, -50%);
+		}
+	</style>
 
-<div class="luxafor-ui-content">
-    <h1 class="heading-text">Luxafor UI</h1>
-    <!-- <a href="https://slack.com/oauth/v2/authorize?client_id=7816636666498.7805940374471&scope=users.profile:read&user_scope=users.profile:read,users.profile:write,users:write"> -->
-    <a href="/oauth/slack">
-    <img
-        style="margin: 20px"
-        alt="Add to Slack" 
-        height="40" 
-        width="139" 
-        src="https://platform.slack-edge.com/img/add_to_slack.png" 
-        srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
-    />
-    </a>
-    <img 
-        src="https://raw.githubusercontent.com/nibrobb/luxafor-ui/refs/heads/slack-integration/public/gui.png"
-        alt="Illustration of the Luxafor-ui app"
-    >
-
-</div>
+	<div class="luxafor-ui-content">
+		<h1 class="heading-text">Luxafor UI</h1>
+		<!-- <a href="https://slack.com/oauth/v2/authorize?client_id=7816636666498.7805940374471&scope=users.profile:read&user_scope=users.profile:read,users.profile:write,users:write"> -->
+		<a href="/oauth/slack">
+			<img
+				style="margin: 20px"
+				alt="Add to Slack"
+				height="40"
+				width="139"
+				src="https://platform.slack-edge.com/img/add_to_slack.png"
+				srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+			/>
+		</a>
+		<img
+			src="https://raw.githubusercontent.com/nibrobb/luxafor-ui/refs/heads/slack-integration/public/gui.png"
+			alt="Illustration of the Luxafor-ui app"
+		/>
+	</div>
+</main>
--- src/routes/oauth/slack/+server.ts
+++ src/routes/oauth/slack/+server.ts
@@ -3,21 +3,17 @@ import 'dotenv/config';
 import { redirect } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async ({ request }) => {
-    const user_scopes = [
-        "users.profile:read",
-        "users.profile:write"
-    ];
-    const user_scopes_string = user_scopes.join(",");
-    // Redirect the user to slack for authorization,
-    // Slack will upon approval of the app, 
-    // redirect back to our /oauth/slack/callback function
-    redirect(307,
-        `https://slack.com/oauth/v2/authorize?client_id=${
-            process.env.SLACK_CLIENT_ID
-        }&user_scope=${
-            encodeURIComponent(user_scopes_string)
-        }&redirect_uri=${
-            encodeURIComponent(process.env.SLACK_REDIRECT_URI || '')
-        }`
-    );
+	const user_scopes = ['users.profile:read', 'users.profile:write'];
+	const user_scopes_string = user_scopes.join(',');
+	// Redirect the user to slack for authorization,
+	// Slack will upon approval of the app,
+	// redirect back to our /oauth/slack/callback function
+	redirect(
+		307,
+		`https://slack.com/oauth/v2/authorize?client_id=${
+			process.env.SLACK_CLIENT_ID
+		}&user_scope=${encodeURIComponent(user_scopes_string)}&redirect_uri=${encodeURIComponent(
+			process.env.SLACK_REDIRECT_URI || ''
+		)}`
+	);
 };

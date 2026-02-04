663971b352a7070d95f13e24e2c667f4b1f4ccf7
Robin <1232179+nibrobb@users.noreply.github.com>
2026-02-04 23:11:53 +0100

    Update +server.ts

--- src/routes/oauth/slack/+server.ts
+++ src/routes/oauth/slack/+server.ts
@@ -3,27 +3,17 @@ import { redirect } from "@sveltejs/kit";
 import { SLACK_CLIENT_ID } from "$env/static/private";
 import { PUBLIC_SLACK_REDIRECT_URI } from "$env/static/public";
 
-export const GET: RequestHandler = async () =>
-// { url }
-{
-    // const session_id = url.searchParams.get("session_id");
-    // const session_secret = url.searchParams.get("session_secret");
-
-    // const state = `${session_id}::${session_secret}`;
-
+export const GET: RequestHandler = async () => {
     const user_scopes = ["users.profile:read", "users.profile:write"];
     const bot_scopes = ["chat:write"];
     const user_scopes_string = user_scopes.join(",");
     const bot_scopes_string = bot_scopes.join(",");
-
     const redirect_url = new URL("https://slack.com/oauth/v2/authorize");
 
     redirect_url.searchParams.set("client_id", SLACK_CLIENT_ID);
     redirect_url.searchParams.set("user_scope", user_scopes_string);
     redirect_url.searchParams.set("scope", bot_scopes_string);
     redirect_url.searchParams.set("redirect_uri", PUBLIC_SLACK_REDIRECT_URI);
-
-    // redirect_url.searchParams.set("state", state);
 
     console.debug(`Redirecting to: ${redirect_url}`);
     redirect(302, redirect_url);

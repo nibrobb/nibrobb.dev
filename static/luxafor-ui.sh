2e09577d24893bed248de9f4d0ba62ccc0b1462b
Robin Kristiansen <1232179+nibrobb@users.noreply.github.com>
2026-04-03 23:04:11 +0200

    Move file, refactor a bit

--- static/luxafor-ui.sh
+++ static/luxafor-ui.sh
@@ -1,21 +1,27 @@
 #!/usr/bin/env sh
+# This script assumes is is being run as root,
+#   and that the system has `curl` and `gnupg` installed
 
 if [ $(id -u) -ne 0 ]; then
-    echo "This script myst be run as root"
+    echo "This script must be run as root"
     exit 1
 fi
 
 keyring_url=https://github.com/nibrobb/luxafor-ui/releases/download/debian/luxafor-ui-archive-keyring.asc
 keyring_path=/etc/apt/keyrings/luxafor-ui-archive-keyring.pgp
 
-curl -fsSL $keyring_url | gpg --dearmor > $keyring_path
+mkdir -p /etc/apt/keyrings # Might not exist on some systems
+
+if ! curl -fsSL "$keyring_url" | gpg --dearmor > "$keyring_path"; then
+    echo "Failed to install Luxafor-UI archive keyring"
+    exit 1
+fi
 
 cat <<EOF > /etc/apt/sources.list.d/luxafor-ui.sources
 Types: deb
 URIs: https://github.com/nibrobb/luxafor-ui/releases/download/debian/
 Suites: ./
-Components: 
 Signed-By: $keyring_path
 EOF
 
-apt update && apt install -y luxafor-ui
+apt-get update && apt-get install -y luxafor-ui

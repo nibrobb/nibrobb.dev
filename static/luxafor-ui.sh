f24b21d10613507003451a70dda53c545432e0dc
Robin <1232179+nibrobb@users.noreply.github.com>
2026-06-04 14:31:19 +0200

    patch: use install command to insert gpg keyring on luxafor-ui script

--- static/luxafor-ui.sh
+++ static/luxafor-ui.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 # This script assumes it is being run as root,
-#   and that the system has `curl` and `gnupg` installed
+#   and that the system has `curl` and `gpg` installed
 
 set -e  # Exit immediately if a command exits with a non-zero status.
 set -u  # Treat unset variables as an error when substituting
@@ -12,10 +12,10 @@ if [ $(id -u) -ne 0 ]; then
 fi
 
 keyring_url=https://github.com/nibrobb/luxafor-ui/releases/download/debian/luxafor-ui-archive-keyring.asc
-keyring_path=/etc/apt/keyrings/luxafor-ui-archive-keyring.pgp
-keyring_tmp=$(mktemp /etc/apt/keyrings/luxafor-ui-archive-keyring.pgp.tmp.XXXXXX)
+keyring_path=/etc/apt/keyrings/luxafor-ui-archive-keyring.gpg
+keyring_tmp=$(mktemp /etc/apt/keyrings/luxafor-ui-archive-keyring.gpg.tmp.XXXXXX)
 
-mkdir -p /etc/apt/keyrings # Might not exist on some systems
+install -m 0755 -d /etc/apt/keyrings # Create directories if the don't exist
 
 if ! curl -fsSL "$keyring_url" | gpg --dearmor > "$keyring_tmp"; then
     rm -f "$keyring_tmp"
@@ -23,8 +23,8 @@ if ! curl -fsSL "$keyring_url" | gpg --dearmor > "$keyring_tmp"; then
     exit 1
 fi
 
-chmod 0644 "$keyring_tmp"
-mv -f "$keyring_tmp" "$keyring_path"
+install -g root -o root -m 0644 "$keyring_tmp" "$keyring_path"
+
 cat <<EOF > /etc/apt/sources.list.d/luxafor-ui.sources
 Types: deb
 URIs: https://github.com/nibrobb/luxafor-ui/releases/download/debian/

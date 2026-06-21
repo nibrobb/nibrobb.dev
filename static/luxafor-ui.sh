278ce382136d85c5cd856a3bd8629eb871b94f5c
Robin <1232179+nibrobb@users.noreply.github.com>
2026-06-21 22:29:34 +0200

    Update luxafor-ui.sh

--- static/luxafor-ui.sh
+++ static/luxafor-ui.sh
@@ -7,6 +7,7 @@ set -u  # Treat unset variables as an error when substituting
 
 keyring_url=https://github.com/nibrobb/luxafor-ui/releases/download/debian/luxafor-ui-archive-keyring.asc
 keyring_path=/etc/apt/keyrings/luxafor-ui-archive-keyring.gpg
+sources_path=/etc/apt/sources.list.d/luxafor-ui.sources
 keyring_tmp=$(mktemp)
 
 sudo install -m 0755 -d /etc/apt/keyrings # Create parent directories
@@ -17,9 +18,11 @@ if ! curl -fsSL "$keyring_url" | gpg --dearmor > "$keyring_tmp"; then
     exit 1
 fi
 
+echo "Installing keyring to $keyring_path"
 sudo install -g root -o root -m 0644 "$keyring_tmp" "$keyring_path"
 
-sudo cat <<EOF > /etc/apt/sources.list.d/luxafor-ui.sources
+echo "Installing source-file to $sources_path"
+cat <<EOF | sudo tee $sources_path
 Types: deb
 URIs: https://github.com/nibrobb/luxafor-ui/releases/download/debian/
 Suites: ./
